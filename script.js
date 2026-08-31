5d231cec7bc84db2f5ab8585044c8b6c5c908819
Shivpratiksha <handeshivpratiksha@gmail.com>
2026-08-31 18:46:24 +0530

    Initial portfolio website

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Portfolio website loaded successfully!");
+
+const year = new Date().getFullYear();
+
+document.querySelector("footer p").textContent =
+    `© ${year} Shivpratiksha. All Rights Reserved.`;
